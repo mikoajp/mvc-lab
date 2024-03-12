33cd0c258c731c4947c559d42a18b3aae5684e04
Mikoaj Przybysz <mprzybysz@digitalvirgo.com>
2024-03-12 17:24:21 +0100

    nodejs mapping + zad 3

---
+++ zad3/student.js
@@ -0,0 +1,15 @@
+const FULLNAME = "Mikołaj Przybysz";
+const STUDENT_ID = 42893;
+
+function getStudentFullName() {
+    return FULLNAME;
+}
+
+function getStudentId() {
+    return STUDENT_ID;
+}
+
+module.exports = {
+    getStudentFullName,
+    getStudentId,
+};
