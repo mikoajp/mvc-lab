bec66c914017f58a1e29dc350f172fecdade0bd3
Mikoaj Przybysz <mprzybysz@digitalvirgo.com>
2024-03-12 17:17:58 +0100

    zad2

---
+++ zad2/student.js
@@ -0,0 +1,12 @@
+const FULLNAME = "Mikołaj Przybysz";
+const STUDENT_ID = 42893;
+
+function getStudentFullName() {
+    return FULLNAME;
+}
+
+function getStudentId() {
+    return STUDENT_ID;
+}
+
+console.log(`My name is ${getStudentFullName()}. My student ID is ${getStudentId()}`);
