5215f1602eb56d79f0fb7de0f7689f6fd2f1f5e0
Mikoaj Przybysz <mprzybysz@digitalvirgo.com>
2024-03-12 17:36:26 +0100

    zad5

---
+++ zad5/app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const student = require('./student');
+
+const PORT = 3000;
+
+const requestListener = function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>About student</title>
+        </head>
+        <body>
+            <p>My name is ${student.getStudentFullName()}. My student ID is ${student.getStudentId()}</p>
+        </body>
+        </html>
+    `);
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
